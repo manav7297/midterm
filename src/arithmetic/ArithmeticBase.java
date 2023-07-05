d042254ffaf143321b0fdabe6fb7c361ffefec4d
manav7297 <manavthakkar1111@gmail.com>
2023-07-05 17:02:36 -0400

    enum commit2

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -1,15 +1,19 @@
 package arithmetic;
 
-import java.util.Scanner;
+import static arithmetic.ArithmeticBase.Operation.DIVIDE;
+import static arithmetic.ArithmeticBase.Operation.MINUS;
+import static arithmetic.ArithmeticBase.Operation.PLUS;
+import static arithmetic.ArithmeticBase.Operation.TIMES;
 
 public class ArithmeticBase {
     public double x, y;
+    private String operation;
 
     public enum Operation {
         PLUS, MINUS, TIMES, DIVIDE
     }
 
-    public double calculate(double x, double y, Operation operation) {
+    public double calculate(int x, int y) {
         switch (operation) {
             case PLUS:
                 return x + y;
@@ -23,3 +27,4 @@ public class ArithmeticBase {
                 throw new AssertionError("Unknown operation: " + operation);
         }
     }
+}
